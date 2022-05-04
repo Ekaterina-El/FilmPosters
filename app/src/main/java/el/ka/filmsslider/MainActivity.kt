69b7b4d66f0d4b8fb0a3881c0eb534f036822818
Ekaterina-El <hackerrussia@yandex.ru>
2022-05-04 17:13:45 +0500

    added auto scrolling

--- app/src/main/java/el/ka/filmsslider/MainActivity.kt
+++ app/src/main/java/el/ka/filmsslider/MainActivity.kt
@@ -1,8 +1,8 @@
 package el.ka.filmsslider
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import el.ka.filmsslider.adapter.SliderAdapter
 import el.ka.filmsslider.common.Common
 import el.ka.filmsslider.models.Movie
@@ -11,9 +11,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var filmsCount = 0
+    private var autoScrollingOn = false
     private lateinit var mService: RetrofitServices
+    private var timer = Timer()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +29,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun getMoviesList() {
-        mService.getMoviesList().enqueue(object: Callback<MutableList<Movie>> {
+        mService.getMoviesList().enqueue(object : Callback<MutableList<Movie>> {
             override fun onResponse(
                 call: Call<MutableList<Movie>>,
                 response: Response<MutableList<Movie>>
@@ -38,7 +42,54 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setMoviesToSlider(movies: MutableList<Movie>) {
+        filmsCount = movies.size
         this.viewPager.adapter = SliderAdapter(this, movies)
         this.indicators.setupWithViewPager(this.viewPager)
+
+        startAutoScrolling()
+
+        this.viewPager.addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state == ViewPager.SCROLL_STATE_DRAGGING && autoScrollingOn) {
+                    timer.cancel()
+                    autoScrollingOn = false
+                } else if (!autoScrollingOn) {
+                    startAutoScrolling()
+                }
+            }
+
+        })
+    }
+
+    private fun startAutoScrolling() {
+        timer = Timer()
+        timer.scheduleAtFixedRate(SliderTimer(), 3000, 3000)
+        autoScrollingOn = true
+    }
+
+    inner class SliderTimer : TimerTask() {
+        override fun run() {
+            this@MainActivity.runOnUiThread {
+                this@MainActivity.viewPager.currentItem =
+                    if (this@MainActivity.viewPager.currentItem < filmsCount - 1) {
+                        this@MainActivity.viewPager.currentItem + 1
+                    } else {
+                        0
+                    }
+
+            }
+        }
+
     }
 }
